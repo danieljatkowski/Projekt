a1a4f8cdf7697358f779a3a80a2dbf214e674ed8
danieljatkowski <daniel.jatkowski24@gmail.com>
2023-02-03 21:36:30 +0100

    Zadanie do wykonania. Imię, wiek, płeć.

--- Aplikacja/Aplikacja/Program.cs
+++ Aplikacja/Aplikacja/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Daniel";
+var sex = "male";
+var age = 26;
+
+if (age > 30)
+{
+    Console.WriteLine("Facet po 30-tce");
+}
+else if (age == 29)
+{
+    if (age == 30)
+    {
+        Console.WriteLine("Trzydziesto letni mężczyzna");
+
+    }
+    else
+    {
+        Console.WriteLine("Dwudziestodziewięcio letni mężczyzna");
+    }
+}
+else
+{
+    Console.WriteLine("Mężczyzna poniżej 29-go roku życia");
+    if(name == "Daniel" && sex == "male")
+    {
+        Console.WriteLine("Cześć Daniel. Miło mi Cię poznać.");
+    }
+}
