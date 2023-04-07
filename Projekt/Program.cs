008088344c0bd2f2de2e4dcc6d82da658e51f4a6
danieljatkowski <daniel.jatkowski24@gmail.com>
2023-04-08 01:37:08 +0200

    Zliczanie cyfr z tablicy char

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,14 +1,19 @@
-﻿List<string> dayOfWeek = new List<string>();
-dayOfWeek.Add("Poniedziałek");
-dayOfWeek.Add("Wtorek");
-dayOfWeek.Add("Środa");
-dayOfWeek.Add("Czwartek");
-dayOfWeek.Add("Piątek");
-dayOfWeek.Add("Sobota");
-dayOfWeek.Add("Niedziela");
+﻿var number = "4566";
 
-foreach (var day in dayOfWeek)
+char[] letters = number.ToArray();
+
+char[] cx = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int[] result = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int i = 0;
+
+foreach (char cxi in cx)
 {
-    Console.WriteLine(day);
+    foreach (char chl in letters)
+    {
+        if (cxi == chl) { result[i]++; }
+    }
+    Console.WriteLine(i + " ==> " + result[i]);
+    i++;
 }
+
 
