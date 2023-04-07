3e9b24b85370686c1f7d75ee1833561f744b7861
danieljatkowski <daniel.jatkowski24@gmail.com>
2023-04-08 00:30:29 +0200

    Lista dni

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> dayOfWeek = new List<string>();
+dayOfWeek.Add("Poniedziałek");
+dayOfWeek.Add("Wtorek");
+dayOfWeek.Add("Środa");
+dayOfWeek.Add("Czwartek");
+dayOfWeek.Add("Piątek");
+dayOfWeek.Add("Sobota");
+dayOfWeek.Add("Niedziela");
+
+foreach (var day in dayOfWeek)
+{
+    Console.WriteLine(day);
+}
+
