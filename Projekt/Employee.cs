05fbf35f633a0d3086bafd56d111b70c50374608
danieljatkowski <daniel.jatkowski24@gmail.com>
2023-04-10 17:03:53 +0200

    Największa ilość pkt pracownika

---
+++ Projekt/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace Projekt
+{
+    public class Employee
+    {
+        private List<int> scores = new List<int>();
+        public string Name
+        {
+            get; private set;
+        }
+        public string Surname
+        {
+            get; private set;
+        }
+        public int Age
+        {
+            get; private set;
+        }
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string AddScore(int score)
+        {
+            if (score < 1 || score > 10)
+            {
+                return "Możesz dodać ocenę od 1 do 10";
+            }
+            this.scores.Add(score);
+            return "Ocena dodana" + score;
+
+        }
+
+        public int ScoreResult
+        {
+            get
+            {
+                return scores.Sum();
+            }
+        }
+
+    }
+}
--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,19 +1,34 @@
-﻿var number = "4566";
+﻿using Projekt;
+using System.Threading.Tasks.Sources;
 
-char[] letters = number.ToArray();
+Employee employee1 = new Employee("Tomasz", "Kozłowski", 34);
+Employee employee2 = new Employee("Jan", "Poniatowski", 53);
+Employee employee3 = new Employee("Grzegorz", "Brzęczyszczykiewicz", 44);
 
-char[] cx = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-int[] result = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-int i = 0;
-
-foreach (char cxi in cx)
+Random random = new Random();
+for (int i = 0; i < 3; i++)
 {
-    foreach (char chl in letters)
-    {
-        if (cxi == chl) { result[i]++; }
-    }
-    Console.WriteLine(i + " ==> " + result[i]);
-    i++;
+    employee1.AddScore(random.Next(1, 11));
+    employee2.AddScore(random.Next(1, 11));
+    employee3.AddScore(random.Next(1, 11));
 }
 
 
+
+if (employee1.ScoreResult > employee2.ScoreResult && employee1.ScoreResult > employee3.ScoreResult)
+{
+    Console.WriteLine("Pan/i " + employee1.Name + " " + employee1.Surname + " " + employee1.Age + " zdobył/a największą liczbę punktów bo aż " + employee1.ScoreResult);
+}
+else if (employee2.ScoreResult > employee1.ScoreResult && employee2.ScoreResult > employee3.ScoreResult)
+{
+    Console.WriteLine("Pan/i " + employee2.Name + " " + employee2.Surname + " " + employee2.Age + " zdobył/a największą liczbę punktówbo aż " + employee2.ScoreResult);
+}
+else if (employee3.ScoreResult > employee1.ScoreResult && employee3.ScoreResult > employee2.ScoreResult)
+{
+    Console.WriteLine("Pan/i " + employee3.Name + " " + employee3.Surname + " " + employee3.Age + " zdobył/a największą liczbę punktów bo aż " + employee3.ScoreResult);
+
+}
+else
+{
+    Console.Write("Z powodu EX AEQUO brak pracownika z największą ilością punktów");
+}
